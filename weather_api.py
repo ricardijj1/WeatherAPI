ca9cf63f9e2b9ce65660654ff13f43259505a69d
Ricardi Jean <ricardijacquet@gmail.com>
2025-01-20 23:56:55 -0500

    final

--- weather_api.py
+++ weather_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pandas as pd
-import numpy as np 
+
 
 url="https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure"
 response = requests.get(url)
@@ -13,19 +13,19 @@ file_path = "data/json/response.json"
 with open(file_path, "w") as json_file:
 #save json file 
     json.dump(weather_data, json_file, indent=4)  
-
 print(f"JSON data saved to {file_path}")
 
-
-#create  dictionnary filtler wanted columns 
+ #create new dictionary filter  wanted columns 
 new_weather_data = {"time": weather_data["hourly"]["time"],
                     "temperature": weather_data["hourly"]["temperature_2m"],
     "relative humidity": weather_data["hourly"]["relative_humidity_2m"],
     "precipitation": weather_data["hourly"]["precipitation"],
     "surface pressure": weather_data["hourly"]["surface_pressure"]}
+#create data frame
 df = pd.DataFrame(new_weather_data)
-
-df.to_csv("data/csv/weather_data_filtered.csv")
+df = df.dropna()
+#save csv file to csv folder 
+df.to_csv("data/csv/weather_data_filtered.csv", index=False)
 
 
 
